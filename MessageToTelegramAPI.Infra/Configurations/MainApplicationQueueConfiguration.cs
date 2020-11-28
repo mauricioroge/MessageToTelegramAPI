55d6c260a11be729f7cc06bdb053de9772a85c0f
Mauricio Rogerio <mauricio_rogerio_@hotmail.com>
2020-11-28 17:55:25 -0300

    Add initial folder structure and PUB/SUB using RabbitMQ

---
+++ MessageToTelegramAPI.Infra/Configurations/MainApplicationQueueConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace MessageToTelegramAPI.Infra.Configurations
+{
+    public class MainApplicationQueueConfiguration
+    {
+
+        public string QueueName { get; set; }
+        public bool Durable { get; set; }
+        public string ExchangeName { get; set; }
+        public string RoutingKey { get; set; }
+        public string ExchangeType { get; set; }
+        public bool Exclusive { get; set; }
+        public bool AutoDelete { get; set; }
+
+
+    }
+}
---
+++ MessageToTelegramAPI.Infra/RabbitMQ/Interfaces/IRabbitMQContext.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace MessageToTelegramAPI.Infra.RabbitMQ.Interfaces
+{
+    public interface IRabbitMQContext : IDisposable
+    {
+        IConnection Connection { get; }
+    }
+}
